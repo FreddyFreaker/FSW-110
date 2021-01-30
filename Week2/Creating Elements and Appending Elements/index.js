a0acca963ccda2f54bddee6dc8dc231bbb638955
FreddyFreaker <74995142+FreddyFreaker@users.noreply.github.com>
2021-01-29 18:24:03 -0600

    Add files via upload

--- Week2/Creating Elements and Appending Elements/index.js
+++ Week2/Creating Elements and Appending Elements/index.js
@@ -1,11 +1,20 @@
 var newListItem = document.createElement('ol')
-newListItem.textContent = 'test'
+
 var myList = document.getElementById('my-list')
 
 var newItem = document.createElement('li')
+newItem.textContent='ice'
 
+var newItem2 = document.createElement('li')
+newItem2.textContent='water'
+
+var newItem3 = document.createElement('li')
+newItem3.textContent='fire'
+
+newItem.appendChild(newItem2)
+newItem.appendChild(newItem3)
 myList.appendChild(newListItem)
-
+newListItem.prepend(newItem)
 
 
 
