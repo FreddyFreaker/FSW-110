84108294f7b46e728dc16448bc88825c5df19f0b
FreddyFreaker <74995142+FreddyFreaker@users.noreply.github.com>
2021-02-19 16:58:28 -0600

    Add files via upload

---
+++ Week 5/Modifying an Element’s Value/index.js
@@ -0,0 +1,9 @@
+function display()
+{
+var x=document.getElementById("input1").value;
+var y=document.getElementById("input2").value;
+var z=document.getElementById("input3").value;
+alert("First input: " + x + " " + "Second input: " + y + " " +"Third input: " + z + " ");
+
+
+}
